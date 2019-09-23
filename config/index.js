4efa9c00ec149492c66410d98ef7320d3ac1d837
Scott <scottbryan17@gmail.com>
2019-09-23 18:56:02 +0800

    changes added to config,app.json,package.json
    - version changed for sdk and app
    - ip changed

--- config/index.js
+++ config/index.js
@@ -7,6 +7,6 @@
 let dev = "http://165.22.245.137";
 let prod = "http://165.22.245.137";
 // let url = "http://192.168.1.2:8080/api";
-let url = "http://202.186.85.15/api";
+let url = "http://18.140.57.209/api";
 
 export { dev, prod, url };
